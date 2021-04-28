126d561ed7ab5ece507cc2f59189f4c3db93ec90
userselu <52342206+SilvinPradhan@users.noreply.github.com>
2021-04-28 17:44:36 -0500

    first commit

---
+++ components/auth/Splash.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button, Text, View} from "react-native-web";
+
+export default function landing({navigation}) {
+    return (
+        <View style={{flex: 1, justifyContent: 'center'}}>
+            <Text>FastConnect</Text>
+            <Button title={"Register"} onPress={() => navigation.navigate('Register')}/>
+            <Button title={"Login"} onPress={() => navigation.navigate('Login')}/>
+        </View>
+    )
+}
